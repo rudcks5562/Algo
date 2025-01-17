c0aa54c266b45c6d50eab9a9a34d3ee0ec704427
rudcks5562 <72645618+rudcks5562@users.noreply.github.com>
2025-01-18 08:54:21 +0900

    [Gold V] Title: 토마토, Time: 96 ms, Memory: 7640 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.cc
+++ 백준/Gold/7569. 토마토/토마토.cc
@@ -9,9 +9,14 @@
 #include"memory.h"
 
 using namespace std;
-int BFS(int M, int N, int H);
+int BFS();
 int maps[101][101][101];
-
+int dx[] = { 1,-1,0,0,0,0 };
+int dy[] = { 0,0,1,-1,0,0 };
+int dz[] = { 0,0,0,0,1,-1 };
+int M, N, H;
+int checker = 0;//1 개수
+int minusChecker=0;
 //priority_queue<pair<int, pair<int, int>>> pq;// h,y,x;
 queue<pair<int, pair<int, int>>> q;
 
@@ -19,12 +24,10 @@ int main()
 {
     cin.tie(0)->sync_with_stdio(0);
 
-    memset(maps, 0, sizeof(maps));
+    //memset(maps, 0, sizeof(maps));
 
-    int M, N, H;
+
     cin >> M >> N >> H;
-    int checker = 0;//1 개수
-    int minusChecker = 0;
 
     for (int s = 0; s < H; s++) {
 
@@ -49,7 +52,7 @@ int main()
     }
 
 
-    int ans=BFS(M, N, H);
+    int ans=BFS();
 
     if (ans == -1) {
         cout << "-1";
@@ -63,19 +66,18 @@ int main()
     return 0;
 }
 
-int BFS(int M, int N, int H)
+int BFS()
 {
-    int dx[] = { 1,-1,0,0,0,0 };
-    int dy[] = { 0,0,1,-1,0,0 };
-    int dz[] = { 0,0,0,0,1,-1 };
+
     int depth = 0;
+    int cnt = 0;
 
     while (!q.empty()) {
 
         int sz = q.size();
        // cout << "sz is= " << sz<<endl;
         for (int s = 0; s < sz; s++) {
-            auto cur = q.front();
+            pair<int,pair<int,int>> cur = q.front();
             int cur_h, cur_x, cur_y;
             cur_h = cur.first;
             cur_y = cur.second.first;
@@ -91,12 +93,12 @@ int BFS(int M, int N, int H)
                 if (next_x < 0 || next_y < 0 || next_z < 0 || next_x >= M || next_y >= N || next_z >= H) {// array bound check
                     continue;
                 }
-                if (maps[next_z][next_y][next_x] == 1 || maps[next_z][next_y][next_x] == -1) {// 이미 익은 토마토이거나(방문됨) 도달못하는 곳 체크
+                if (maps[next_z][next_y][next_x] !=0) {// 이미 익은 토마토이거나(방문됨) 도달못하는 곳 체크
                     continue;
                 }
                 maps[next_z][next_y][next_x] = 1;// 익음
                 q.push(make_pair(next_z, make_pair(next_y, next_x)));
-
+                cnt++;
 
             }
             /*
@@ -130,19 +132,16 @@ int BFS(int M, int N, int H)
 
         }
     // 모두 익은건지 아닌지 체크
-    for (int s = 0; s < H; s++) {
 
-        for (int i = 0; i < N; i++) {
+    if (M * N * H - minusChecker != cnt + checker) {
 
-            for (int j = 0; j < M; j++) {
+        return -1;
 
-                if (maps[s][i][j] == 0) {
-                    return -1;
+    }
 
-                }
-            }
-        }
-    }// input end
+
+
+
 
 
     return depth;
